90a92c1bfcea482ce31453756bb03068383f856b
humblewasp <huang030225@gmail.com>
2026-09-17 19:42:48 +0800

    Remove ECNU education and update the resume PDF

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jason-huang-books-v7";
+const CACHE_NAME = "jason-huang-books-v8";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add(self.registration.scope)));
